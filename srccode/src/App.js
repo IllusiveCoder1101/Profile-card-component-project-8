d0c1889bc18e3045339ca1ff89926a77acae2ba2
Sankalpa Sarkar <codbegtocodpro@gmail.com>
2022-09-05 17:37:25 +0530

    Fixed background layouting.

--- srccode/src/App.js
+++ srccode/src/App.js
@@ -2,12 +2,8 @@ import React from 'react'
 
 function App() {
   return (
-    <div>
-      <div className="top">
-        <svg width="978" height="978" xmlns="http://www.w3.org/2000/svg"><defs><linearGradient x1="50%" y1=".779%" x2="50%" y2="100%" id="a"><stop stopColor="#0989B4" stopOpacity="0" offset="0%"/><stop stopColor="#53FFEE" offset="99.94%"/></linearGradient></defs><ellipse fill="url(#a)" transform="rotate(-135 489 489)" cx="489" cy="489" rx="489" ry="488" fillRule="evenodd" opacity=".5"/></svg>
-      </div>
-      <div className="box">
-      <div className="mid">
+    <div className='box'>
+      <div className="container">
         <div className="header">
         </div>
         <img src={require("./assets/image-victor.jpg")} alt="avatar" className='avatar' />
@@ -26,11 +22,7 @@ function App() {
             <h2 className='no'>1.4k</h2>
             <p className='tag'>Photos</p>
           </div>
-        </div>
       </div>
-      </div>
-      <div className="bottom">
-        <svg width="978" height="978" xmlns="http://www.w3.org/2000/svg"><defs><linearGradient x1="50%" y1=".779%" x2="50%" y2="100%" id="a"><stop stopColor="#0989B4" stopOpacity="0" offset="0%"/><stop stopColor="#53FFEE" offset="99.94%"/></linearGradient></defs><ellipse fill="url(#a)" transform="scale(1 -1) rotate(45 1669.55 0)" cx="489" cy="489" rx="489" ry="488" fillRule="evenodd" opacity=".25"/></svg>
       </div>
 
     </div>
